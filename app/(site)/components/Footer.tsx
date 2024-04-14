93b5d695ce107e2b6ef6089bcaec92f02e967331
ABC1319 <109276671+ABC1319@users.noreply.github.com>
2024-04-14 18:14:12 +0800

    Update Footer.tsx

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <div className="fixed bottom-0 bg-black text-gray-400 w-full bg-opacity-50 h-7 md:h-10 flex justify-center items-center text-sm md:text-base cursor-default">
-      <span>Since 2023 李文凯 &</span>
+      <span>Since 2021-2024 Sky &</span>
       <Link
-        href="https://github.com/liwenka1/next-home"
+        href="https://github.com/"
         target="_blank"
         className="px-1 hover:text-white transition-colors"
       >
-        Liwenka1
+        Sky
       </Link>
       <span>All Rights Reserved</span>
     </div>
