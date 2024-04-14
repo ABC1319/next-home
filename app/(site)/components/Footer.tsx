1cc602f66957807376c07ee30158d214055e4325
ABC1319 <109276671+ABC1319@users.noreply.github.com>
2024-04-14 19:48:05 +0800

    Update Footer.tsx

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="fixed bottom-0 bg-black text-gray-400 w-full bg-opacity-50 h-7 md:h-10 flex justify-center items-center text-sm md:text-base cursor-default">
       <span>Since 2021-2024 Sky &</span>
       <Link
-        href="https://github.com/ABC1319/next-home"
+        href="https://github.com/"
         target="_blank"
         className="px-1 hover:text-white transition-colors"
       >
